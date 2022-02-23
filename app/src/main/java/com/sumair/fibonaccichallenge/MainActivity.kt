7f0604c448e0533438da78b8f98606b98eafcd10
Sumair <syed.sumair@here.com>
2022-02-23 09:52:40 +0100

    fix: solution for fibonacci

--- app/src/main/java/com/sumair/fibonaccichallenge/MainActivity.kt
+++ app/src/main/java/com/sumair/fibonaccichallenge/MainActivity.kt
@@ -12,49 +12,62 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
-    private var mRecyclerView: RecyclerView? = null
+    private lateinit var mRecyclerView: RecyclerView
 
-    var fibonaccilist = mutableListOf<Long>()
+    private var fibonacciList = mutableListOf<Long>()
+
+    private val steps: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initViews()
-        fibonacci(Long.MAX_VALUE)
-        val mLayoutManager = LinearLayoutManager(this)
-        val recyclerViewAdapter = RecyclerViewAdapter(fibonaccilist);
-        mRecyclerView!!.layoutManager = mLayoutManager
-        mRecyclerView!!.itemAnimator = DefaultItemAnimator()
-        mRecyclerView!!.adapter = recyclerViewAdapter
-        mRecyclerView!!.adapter?.notifyDataSetChanged()
-        mRecyclerView!!.addOnScrollListener(object : RecyclerView.OnScrollListener() {
 
+        loadFibonacci()
+
+        val mLayoutManager = LinearLayoutManager(this)
+        val recyclerViewAdapter = RecyclerViewAdapter(fibonacciList)
+        mRecyclerView.layoutManager = mLayoutManager
+        mRecyclerView.itemAnimator = DefaultItemAnimator()
+        mRecyclerView.adapter = recyclerViewAdapter
+        mRecyclerView.adapter?.notifyItemChanged(fibonacciList.size)
+        mRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
                 if (mLayoutManager.findLastCompletelyVisibleItemPosition() >= mLayoutManager.itemCount - 1) {
-                    recyclerViewAdapter.addItems(fibonaccilist)
+                    loadFibonacci()
                 }
             }
         })
-
-    }
-
-    private fun fibonacci(n: Long) = GlobalScope.launch(Dispatchers.IO) {
-        withContext(Dispatchers.Main) {
-            fibonaccilist.add(n)
-        }
-        if (n > 1){
-            fibonacci(n - 1) + fibonacci(n - 2)
-        }
-
     }
 
     private fun initViews() {
-        mRecyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        mRecyclerView = findViewById(R.id.recyclerView)
 
     }
 
+    private fun loadFibonacci() = GlobalScope.launch(Dispatchers.Default) {
+        var n = fibonacciList.size.toLong()
+        val tempList: MutableList<Long> = arrayListOf()
+        while (n < steps + fibonacciList.size && steps + fibonacciList.size < Long.MAX_VALUE) {
+            val result = fib(n)
+            tempList.add(result)
+            Thread.sleep(100)
+            n++
+        }
+
+        withContext(Dispatchers.Main) {
+            val adapter = mRecyclerView.adapter as RecyclerViewAdapter
+            adapter.addItems(tempList)
+        }
+
+    }
+
+    private fun fib(n: Long): Long {
+        return if (n <= 1) n
+        else fib(n - 1) + fib(n - 2)
+    }
 
 
 }
--- app/src/main/java/com/sumair/fibonaccichallenge/ui/RecyclerViewAdapter.kt
+++ app/src/main/java/com/sumair/fibonaccichallenge/ui/RecyclerViewAdapter.kt
@@ -23,22 +23,19 @@ class RecyclerViewAdapter(private var list: MutableList<Long>) : RecyclerView.Ad
     // binding data to each cell
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         holder as RegularViewHolder
-        holder.febonacci_no.text = list[position].toString()
+        holder.fibonacciNo.text = list[position].toString()
     }
 
     // add items to recycler view
     fun addItems(moreItems: MutableList<Long>) {
         val positionStart = list.size + 1
         list +=moreItems
-        notifyItemRangeInserted(positionStart, moreItems.size);
+        notifyItemRangeInserted(positionStart, moreItems.size)
     }
 
     inner class RegularViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        var febonacci_no: TextView
-
-        init {
-            febonacci_no = itemView.findViewById<View>(R.id.fibonacci_no) as TextView
-        }
+        var fibonacciNo:
+                TextView = itemView.findViewById<View>(R.id.fibonacci_no) as TextView
 
     }
 
